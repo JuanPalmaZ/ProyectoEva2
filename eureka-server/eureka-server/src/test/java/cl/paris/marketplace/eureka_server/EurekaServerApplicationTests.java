b2a02a33df41de5fdc336a7643c45a84f17c4b07
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-18 14:18:11 -0400

    Erika sin config

---
+++ eureka-server/eureka-server/src/test/java/cl/paris/marketplace/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.paris.marketplace.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
