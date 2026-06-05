048303ae2155390a47084b84820f4138ebf07652
JuanPalmaZ <juan.zapatac4@gmail.com>
2026-06-05 00:13:27 -0400

    ahora si legaxy

--- ms.legacy/src/main/java/cl/paris/marketplace/ms/legacy/controller/LegacyController.java
+++ ms.legacy/src/main/java/cl/paris/marketplace/ms/legacy/controller/LegacyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/legacy")
+@PreAuthorize("isAuthenticated()")
 public class LegacyController {
 
     private final LegacyService legacyService;
